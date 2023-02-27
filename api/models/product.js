cdb6219f263f2b2171da9be1991d235b488ef0de
Gautam Patel <gautam.patel.5317@gmail.com>
2023-02-28 01:08:12 +0530

    signup,login and authorization

--- api/models/product.js
+++ api/models/product.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
         _id: mongoose.Schema.Types.ObjectId,
-        name: {type: String, required: true},
-        price: {type: Number, required: true},
+        name: {
+                type: String,
+                required: true
+        },
+        price: {
+                type: Number,
+                required: true
+        },
+        productImage: {
+                type: String,
+                required: true
+        }
 });
-module.exports = mongoose.model('Product',productSchema);
+module.exports = mongoose.model('Product', productSchema);
