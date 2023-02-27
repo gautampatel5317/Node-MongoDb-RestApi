383f8cc782d97167c9d33b315c00cae783594180
Gautam Patel <gautam.patel.5317@gmail.com>
2023-02-27 10:38:09 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+const productRoutes = require('./api/routes/products');
+const orderRoutes   = require('./api/routes/orders');
+const uri = `mongodb+srv://gautampatel5317:${process.env.MONGO_ATLAS_PW}@node-rest-shop.84vhydv.mongodb.net/test?retryWrites=true&w=majority`;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB', error);
+  });
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,Authorization');
+    if (req.method === 'OPTIONS') {
+        req.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+app.use('/products',productRoutes);
+app.use('/orders',orderRoutes);
+app.use((req,res,next) => {
+    const error = new Error('Not Found!');
+    error.status = 404;
+    next(error);
+});
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+module.exports = app;
